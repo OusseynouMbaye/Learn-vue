0c0c057c3e7cd49e8bff6a87ddc78c4145e62489
Ouzinem <ouzinem@hotmail.com>
2021-10-15 12:09:26 -0400

    v-bind class en cours

--- learn-vue/src/App.vue
+++ learn-vue/src/App.vue
@@ -2,8 +2,12 @@
 <h1 v-bind:title="sport"> hello {{name}}</h1>
 <div v-text="sport"></div>
 <div> allo <b v-html="series"> </b></div>
+<!-- binding attribute to use id -->
 <h2 v-bind:id="headingId"> Heading</h2>
+<!-- binding to use boolean -->
 <button v-bind:disabled="isDisabled">texte desactive</button>
+<h2 class="underline"> underline texte</h2>
+<h3 v-bind:class="status">ok</h3>
 </template>
 
 <script>
@@ -17,7 +21,8 @@ export default {
       sport : 'Swimming',
       series : "NCIS",
       headingId : "heading",
-      isDisabled : false
+      isDisabled : false,
+      status : "danger",
     }
   }
 }
@@ -31,5 +36,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.underline{
+  text-decoration: underline;
 }
 </style>
